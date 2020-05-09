dca68006fa590969b9dec23114f87216c359313b
Ethan's Mac <ethanphanvn@gmail.com>
2020-05-09 08:01:15 -0400

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todo';
+  title = "To-do list";
+  name="Ethan"
+
+  constructor() {
+    this.changeName('John')
+  }
+
+  changeName(name:string):void {
+    this.name = name
+  }
 }
